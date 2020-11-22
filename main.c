a5d5dd8552380a2072a84713bb445d3798bb9504
spopsin <spopsin@idejr.com.br>
2020-11-21 21:00:13 -0300

    main to AVL adaptation

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main (int argc, char *argv[])
 {
     FILE *entrada;
     FILE *saida;
-    DicLDE *D, *posicao;
+    DicAVL *D, *posicao;
     char palavra[30], tab;
 
     if (argc!=4)//caso seja passado um n�mero menor/maior de argumentos na linha de comando
@@ -20,7 +20,7 @@ int main (int argc, char *argv[])
     }
     else//caso seja passado exatos 4 argumentos
     {
-        D=recebeDicionario(argv[1]);//passa argumento 2(nome do dicionario) para fun��o que o coloca em uma LDE
+        D=recebeDicionarioAVL(argv[1]);//passa argumento 2(nome do dicionario) para fun��o que o coloca em uma AVL
         if((entrada=fopen(argv[2],"r")) == NULL){//abre arquivo de entrada(texto a ser lematizado) e checa se ele existe
             printf("Nao foi possivel ler arquivo de entrada");
             return 2;
@@ -28,8 +28,8 @@ int main (int argc, char *argv[])
         saida=fopen(argv[3],"a+");//abre arquivo de saada com nome disponibilizado pela linha de comando
         while(!feof(entrada)){//enquanto n�o chega no final do arquivo de entrada(texto a ser lematizado)
             strcpy(palavra,ler_arquivo(entrada,palavra,&tab));//chama fun��o que l� palavra do arquivo e a copia para vari�vel local
-            posicao=encontraNoDic(D,palavra,&comp);                                     //chama fun��o que procura posi��o da palavra na lista(se existir) e a devolve
-            colocaSaida(saida,posicao,palavra,tab);                                     //chama fun��o que coloca lema(se achado) no arquivo de sa�da
+            posicao=encontraNoDicAVL(D,palavra,&comp);                                     //chama fun��o que procura posi��o da palavra na lista(se existir) e a devolve
+            colocaSaidaAVL(saida,posicao,palavra,tab);                                     //chama fun��o que coloca lema(se achado) no arquivo de sa�da
         }
         fclose(entrada);                                                                //fecha arquivos abertos
         fclose(saida);
